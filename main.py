8b0ea33393a24c9b5d189728f0c7248e50bd96ec
Oleksii Usatov <usatovalexey81@gmail.com>
2022-05-01 12:34:53 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from cv2 import cv2
 import sys
-#colorset = "MWN$@%#&B89EGA6mK5HRkbYT43V0JL7gpaseyxznocv?jIftr1li*=-~^`':;,. "
-colorset = "MWN$@%#&B8gpasenocv?jIli*=-~^`. "
+
+# colorset_64 = "MWN$@%#&B89EGA6mK5HRkbYT43V0JL7gpaseyxznocv?jIftr1li*=-~^`':;,. "
+colorset_32 = "MWN$@%#&B8gpasenocv?jIli*=-~^`. "
 
 if __name__ == "__main__":
 
-    # define a video capture object
+    # Define a video capture object
     vid = cv2.VideoCapture(0)
 
     while True:
@@ -17,20 +18,21 @@ if __name__ == "__main__":
         if not ret:
             break
 
-        frame = cv2.resize(frame, (0,0), fx = 0.08,  fy = 0.08)
+        frame = cv2.resize(frame, (0, 0), fx=0.08, fy=0.08)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         output = ""
 
         for gray2 in gray:
             output += "\n"
             for dark in gray2:
-                #output += colorset[dark // 4] * 2
-                output += colorset[dark // 8] * 2
+                # output += colorset_64[dark // 4] * 2
+                output += colorset_32[dark // 8] * 2
 
         print('\r' + output)
         sys.stdout.flush()
 
     # After the loop release the cap object
     vid.release()
+
     # Destroy all the windows
     cv2.destroyAllWindows()
